12ae6020abc8ab902c6862815ecfeb7d8eda415e
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2016-02-08 17:26:23 -0300

    Fixing name of the view

--- votai_theme/views.py
+++ votai_theme/views.py
@@ -1,8 +1,7 @@
 from elections.views import SoulMateDetailView
 
 
-class MediaNarajaView(SoulMateDetailView):
+class MediaNaranjaView(SoulMateDetailView):
     def get_information_holder(self, data={}):
         holder = super(MediaNarajaView, self).get_information_holder(data)
-        print "Media Naranja ah!"
         return holder
